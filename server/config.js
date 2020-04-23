a8f03760c8e58c42fe7958dba074ff420dd94dfb
krishna M <krishnamurugan589@gmail.com>
2020-04-23 14:57:45 +0530

    (migrated.) backend to server to new repo. (movie_app_backend)[https://github.com/M-krishna/movie_app_backend]

--- server/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    database: "mongodb://localhost:27017/movies",
-    secret: 'secret12345'
-}
--- server/server.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-const app = require('./app');
-const port = process.env.PORT || 5000;
-const server = http.createServer(app);
-server.listen(port, () => {
-    console.log(`Server started at port ${port}`);
-});
